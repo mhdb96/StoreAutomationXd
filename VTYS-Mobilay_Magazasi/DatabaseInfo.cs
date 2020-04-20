cfd4d2c1c2f205e76111d0998b759277b49e1403
mhdb96 <mhd.b96@gmail.com>
2020-04-20 20:21:36 +0300

    Deployed to db4free.net for test purposes

--- VTYS-Mobilay_Magazasi/DatabaseInfo.cs
+++ VTYS-Mobilay_Magazasi/DatabaseInfo.cs
@@ -11,22 +11,32 @@ namespace VTYS_Mobilay_Magazasi
     class DatabaseInfo
     {
         //Veritabanının bilgilerini string değişkenlere attı
-        public static string server = "localhost";
-        public static string user = "root";
-        public static string pass = "alpha86";
-        public static string database = "pro";
-        public static uint port = 3306;
-        //
+
+        // db4free.net
+        public static string server = "db4free.net";        
+        public static string user = "db_tester";        
+        public static string pass = "fortesting";        
+        public static string database = "store_auto_db";
+        public static uint port = 3306;        
+        
+        //// Local
+        //public static string server = "localhost";
+        //public static string user = "root";        
+        //public static string pass = "alpha86";        
+        //public static string database = "pro";        
+        //public static uint port = 3307;
+        
         static public MySqlConnection getConnection()
         {
             //MySqlConnectionStringBuilder sınıfından builder nesnesi yaratıp bilgileri içine attı
             MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
             {
                 Server = server,
+                Port = port,
+                Database = database,
                 UserID = user,
                 Password = pass,
-                Database = database,
-                Port = port
+                OldGuids = true
             };
             //
             //build nesnesi yardımıyla MySqlConnection sınıfından myConnection nesnesi oluşturdu
